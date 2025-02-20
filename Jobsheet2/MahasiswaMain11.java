6982a9f8e28929a8f6adc4c048726f67ef41d563
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-20 08:58:14 +0700

    Jobsheet 2 Percobaan 2

--- Jobsheet2/MahasiswaMain11.java
+++ Jobsheet2/MahasiswaMain11.java
@@ -1,6 +1,6 @@
 package Jobsheet2;
 
-public class MahasiswaMain {
+public class MahasiswaMain11 {
     public static void main(String[] args) {
         Mahasiswa11 mhs1 = new Mahasiswa11();
         mhs1.nama = "Muhammad Ali Farhan";
