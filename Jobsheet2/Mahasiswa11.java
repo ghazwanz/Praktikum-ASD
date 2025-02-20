22b815e3c1b0ec1a910a30607595a521dfde7782
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-20 08:45:54 +0700

    Jobsheet 2 Percobaan 1 deklarasi class, method, dan atribut

---
+++ Jobsheet2/Mahasiswa11.java
@@ -0,0 +1,36 @@
+package Jobsheet2;
+
+public class Mahasiswa11 {
+    String nama, nim, kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if(ipkBaru < 0 || ipkBaru > 4) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 - 4.0");
+            return;
+        } else ipk = ipkBaru;
+    }
+
+    String menilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
