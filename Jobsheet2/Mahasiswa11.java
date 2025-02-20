22f84c61d36ddbe7ec15447d7d27644e1770c621
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-20 09:29:54 +0700

    Jobsheet 3 Percobaan 3 Constructor

--- Jobsheet2/Mahasiswa11.java
+++ Jobsheet2/Mahasiswa11.java
@@ -4,6 +4,16 @@ public class Mahasiswa11 {
     String nama, nim, kelas;
     double ipk;
 
+    public Mahasiswa11(){ 
+    }
+    
+    public Mahasiswa11(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
