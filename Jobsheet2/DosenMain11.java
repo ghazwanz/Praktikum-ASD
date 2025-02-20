01f198571814a5fd9243b614c6c31f13b47e1ee5
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-20 12:26:47 +0700

    Jobsheet 2 Update Latihan 2 main

--- Jobsheet2/DosenMain11.java
+++ Jobsheet2/DosenMain11.java
@@ -4,15 +4,16 @@ public class DosenMain11 {
     public static void main(String[] args) {
         Dosen11 ds1 = new Dosen11();
         ds1.idDosen = "VIT";
-        ds1.nama = "Vit Zuraida S.Kom., M.Kom.";
+        ds1.nama = "Vit Zuraida, S.Kom., M.Kom.";
         ds1.statusAktif = true;
         ds1.tahunBergabung = 2020;
         ds1.bidangKeahlian = "Basis Data";
         ds1.tampilInformasi();
         ds1.setStatusAktif(false);
+        ds1.ubahKeahlian("Praktikum Basis Data");
         ds1.tampilInformasi();
 
-        Dosen11 ds2 = new Dosen11("SA", "Samsul Arifin", true, 2023,"Sistem Operasi");
+        Dosen11 ds2 = new Dosen11("SA", "Samsul Arifin, S.Kom., MMSI", true, 2023,"Sistem Operasi");
         ds2.tampilInformasi();
     }    
 }
