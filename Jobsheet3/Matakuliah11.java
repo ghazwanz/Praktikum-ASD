bbdc8c904e36e20d8d190dbe95d65911f5bde1cd
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-27 11:48:51 +0700

    Jobsheet 3 Percobaan 3 Konstruktor Berparameter

---
+++ Jobsheet3/Matakuliah11.java
@@ -0,0 +1,11 @@
+package Jobsheet3;
+public class Matakuliah11 {
+    public String kode,nama;
+    public int sks, jumlahJam;
+    public Matakuliah11(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
