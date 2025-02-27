f6319138479418872283b79223d2d2195cfa18a2
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-02-27 11:07:58 +0700

    Jobsheet 3 Percobaan 2 Pertanyaan

--- Jobsheet3/Mahasiswa11.java
+++ Jobsheet3/Mahasiswa11.java
@@ -3,4 +3,12 @@ package Jobsheet3;
 public class Mahasiswa11 {
     public String nim,nama,kelas;
     public float ipk;
+
+    void cetakInformasi(){
+        System.out.printf("NIM     : %s\n",nim);
+        System.out.printf("Nama    : %s\n",nama);
+        System.out.printf("Kelas   : %s\n",kelas);
+        System.out.printf("IPK     : %s\n",ipk);
+        System.out.println("----------------------------------");
+    }
 }
--- Jobsheet3/MahasiswaDemo11.java
+++ Jobsheet3/MahasiswaDemo11.java
@@ -21,13 +21,10 @@ public class MahasiswaDemo11 {
             arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
             System.out.println("----------------------------------");
         }
-
+        
         for (int i = 0; i < 3; i++) {
-            System.out.println("NIM     : "+arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : "+arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : "+arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : "+arrayOfMahasiswa[i].ipk);
-            System.out.println("----------------------------------");
+            System.out.println("Data Mahasiswa ke-"+(i+1));
+            arrayOfMahasiswa[i].cetakInformasi();
         }
     }
 }
