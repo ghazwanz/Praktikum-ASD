6aea2da3a9748c42ea2e5d44083934c8804efd7f
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-03-20 13:00:14 +0700

    Jobsheet 5 Percobaan 2 Sorting Berdasarkan IPK

---
+++ Jobsheet5/Mahasiswa11.java
@@ -0,0 +1,23 @@
+package Jobsheet5;
+
+public class Mahasiswa11 {
+    String nim,nama, kelas;
+    double ipk;
+
+    Mahasiswa11(){
+    }
+
+    Mahasiswa11(String nim, String nama, String kelas, double ipk){
+        this.nim = nim;
+        this.nama = nama;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
+    void tampilInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
