9c6961f624fe9c4974c8fbfa54c81149efa4ec5f
Ghazwan Ababil <ababilghazwan@gmail.com>
2025-03-21 19:37:12 +0700

    Jobsheet 5 Percobaan 4 Sorting IPK dengan Insertion Sort

--- Jobsheet5/MahasiswaDemo11.java
+++ Jobsheet5/MahasiswaDemo11.java
@@ -21,9 +21,13 @@ public class MahasiswaDemo11 {
         // System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
         // list.bubbleSort();
         // list.tampil();
-        
-        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
-        list.selectionSort();
+
+        // System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        // list.selectionSort();
+        // list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
         list.tampil();
     }
 }
