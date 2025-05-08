45cea448e19b3bf4780c5a2737dc94c23358b9df
ghazwanz <ababilghazwan@gmail.com>
2025-05-08 10:03:41 +0700

    Jobsheet 9 Percobaan 2 Stack Konversi Biner

--- Jobsheet9/MahasiswaDemo11.java
+++ Jobsheet9/MahasiswaDemo11.java
@@ -39,6 +39,8 @@ public class MahasiswaDemo11 {
                         int nilai = scan.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai tugas %s adalah %d\n",dinilai.nama,nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas: " + biner);
                     }
                     break;
                 case 3:
--- Jobsheet9/StackTugasMahasiswa11.java
+++ Jobsheet9/StackTugasMahasiswa11.java
@@ -61,4 +61,17 @@ public class StackTugasMahasiswa11 {
         }
         System.out.println();
     }
+
+    String konversiDesimalKeBiner(int nilai){
+        StackKonversi11 stack = new StackKonversi11();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) 
+            biner += stack.pop();
+        return biner;
+    }
 }
